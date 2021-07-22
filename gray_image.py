a7aa60849654b8c7e3cc08fbf35f073dd7ebe253
NITHIN C <nithinnithin2812@gmail.com>
2021-07-22 11:29:02 +0530

    removed my python file named gray image

--- gray_image.py
+++ gray_image.py
@@ -1,9 +1,23 @@
-from simpleimage import SimpleImag
-import simpleimagee
+from simpleimage import SimpleImage
+img = SimpleImage('C:\\Users\\Nithin\\My_GitHub\\Gray-image-converter\\flower.png')
+img.show()
+pixel=image.get_pixel(x,y)
+average=(pixel.red + pixel.blue + pixel.green)//3
 
-def main():
-    image=simpleimage("flower.png")
+def gray_scale(x):
+    average=(x.red + x.blue + x.green)//3
+    x.red=average
+    x.blue=average
 
 
-if __name__=="__main__":
-    main()
+
+for pixel in img:
+    if pixel.red>average*1.6:
+        pixel.red=255
+        pixel.blue=0
+        pixel.green=0
+    else:
+        gray_scale(pixel)
+
+img.show()
+
